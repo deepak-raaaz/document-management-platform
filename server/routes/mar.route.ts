2fce8e6b0f894ec44658883695aab1b0d939e2b0
AyushABD-17 <ayushraj9122839342@gmail.com>
2024-04-16 21:07:56 +0530

    ...routes

--- server/routes/mar.route.ts
+++ server/routes/mar.route.ts
@@ -1,4 +1,4 @@
-import { deleteMAR, getMarCategories, getMyMar, uploadMAR } from "../controllers/mar.controller";
+import { deleteMAR,  getMyMar, getVerifiedMarCategories, uploadMAR } from "../controllers/mar.controller";
 import { isAuthenticate } from "../middleware/auth";
 import express from "express";
 
@@ -8,7 +8,7 @@ const marRouter = express.Router();
 marRouter.post("/upload-mar", isAuthenticate, uploadMAR);
 marRouter.get("/mar-list", isAuthenticate, getMyMar );
 marRouter.delete("/delete-mar", isAuthenticate, deleteMAR );
-marRouter.get("/mar-category-list", isAuthenticate, getMarCategories );
+marRouter.get("/mar-category-list", isAuthenticate, getVerifiedMarCategories );
 
 
 
