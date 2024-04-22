49853efd8ec8ccc8422080e1bb93500c69259cd1
deepak-raaaz <deepakjamui26@gmail.com>
2024-04-22 23:26:26 +0530

    deploy edit

--- server/app.ts
+++ server/app.ts
@@ -17,8 +17,9 @@ app.use(cookieParser());
 //cors = cross origin resource sharing
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://hit-cse-ds.vercel.app","http://localhost:3000"],
     credentials: true,
+    methods: ["GET","POST","PUT","DELETE"],
   })
 );
 
